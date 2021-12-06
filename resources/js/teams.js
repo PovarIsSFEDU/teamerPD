7f254d3b118a3b15e5dc6a423b6f872ed4d11abc
PovarIsSFEDU <plukash@sfedu.ru>
2021-12-06 21:38:14 +0300

    Links and js correction.

--- resources/js/teams.js
+++ resources/js/teams.js
@@ -1,3 +1,7 @@
+import {sendRequest} from "./sendRequest.js";
+import {ThrowError} from "./throwError.js";
+import {deleteCookie} from "./deleteCookie.js";
+
 var teaminfo = document.getElementById('teaminfo')
 
 teaminfo.addEventListener('show.bs.modal', (event) => {
@@ -8,4 +12,15 @@ teaminfo.addEventListener('show.bs.modal', (event) => {
 
     modalTitle.textContent = teamname
     //modalBody.value = recipient
+
+    const _button_signout = document.querySelector(".signout");
+
+    if (_button_signout != null) {
+        _button_signout.addEventListener("click", () => {
+            sendRequest("GET", "/logout", null)
+                .catch(err => ThrowError(err))
+            deleteCookie("Authenticate")
+            window.location.replace("/login")
+        })
+    }
 })
