4747726ce3f69199a82343541b0d61cff63d9a9e
PovarIsSFEDU <plukash@sfedu.ru>
2021-12-06 17:25:38 +0300

    Незначительное добавление функционала, оптимизация шаблонизатора.

--- resources/js/profile.js
+++ resources/js/profile.js
@@ -1,2 +1,20 @@
+import {sendRequest} from "./sendRequest.js";
+import {send_profile} from "./send_profile.js";
+import {ThrowError} from "./throwError.js";
+import {deleteCookie} from "./deleteCookie.js";
+
 document.addEventListener('DOMContentLoaded', () => {
+    const _button_signout = document.querySelector(".signout");
+
+    if (_button_signout != null) {
+        _button_signout.addEventListener("click", () => {
+            sendRequest("GET", "/logout", null)
+                .catch(err => ThrowError(err))
+            deleteCookie("Authenticate")
+            window.location.replace("/login")
+        })
+    }
+
+    send_profile(document.querySelector("#profile_name").textContent)
+
 });
