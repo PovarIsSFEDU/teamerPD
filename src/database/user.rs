1e0fdf89a676c88f09efafe99b6aff29a9b89774
Lyrai <x355x02c@live.com>
2021-12-10 20:12:54 +0300

    Added user info update

--- src/database/user.rs
+++ src/database/user.rs
@@ -1,14 +1,21 @@
+use rocket::data::{FromData, Outcome};
+use rocket::{Data, Request};
 use crate::auth::RegistrationData;
 use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct User {
+    #[serde(default)]
     pub name: String,
     pub team: Option<String>,
     pub photo: Option<String>,
     pub resume: Option<String>,
+    #[serde(default)]
     pub adm: bool,
-    pub email: String
+    #[serde(default)]
+    pub email: String,
+    #[serde(default)]
+    pub competences: Vec<String>
 }
 
 
@@ -20,7 +27,17 @@ impl User {
             photo: None,
             resume: None,
             adm: false,
-            email: data.email().to_owned()
+            email: data.email().to_owned(),
+            competences: vec![]
         }
+    }
+}
+
+#[async_trait]
+impl<'r> FromData<'r> for User {
+    type Error = ();
+
+    async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> Outcome<'r, Self> {
+        todo!()
     }
 }
