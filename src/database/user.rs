862a73fd413398a05d6482c11bdec4f50b251082
Lyrai <x355x02c@live.com>
2021-12-10 20:20:14 +0300

    Fix

--- src/database/user.rs
+++ src/database/user.rs
@@ -1,7 +1,9 @@
 use rocket::data::{FromData, Outcome};
 use rocket::{Data, Request};
+use rocket::http::Status;
 use crate::auth::RegistrationData;
 use serde::{Serialize, Deserialize};
+use crate::request;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct User {
@@ -38,6 +40,11 @@ impl<'r> FromData<'r> for User {
     type Error = ();
 
     async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> Outcome<'r, Self> {
-        todo!()
+        let body = request::body_to_string(req, data).await;
+        let result = serde_json::from_str::<Self>(body.as_str());
+        match result {
+            Ok(user) => Outcome::Success(user),
+            Err(_) => Outcome::Failure((Status::InternalServerError, ()))
+        }
     }
 }
